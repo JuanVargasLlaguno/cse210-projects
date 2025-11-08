8bc28edf9e09cd32e5a51db21e1355dc78a8f746
JuanVargasLlaguno <jvargasllaguno@byupathway.edu>
2025-11-08 00:05:30 -0300

    Program updated up to Step 5

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job(); //create an object from the "Job" class
+        job1._jobTitle = "Software Engineer"; //enter data into the attributes
+        job1._company = "Apple"; //enter data into the attributes
+        job1._startYear = 2013; //enter data into the attributes
+        job1._endYear = 2021; //enter data into the attributes
+
+        Job job2 = new Job();
+        job2._jobTitle = "Administrative";
+        job2._company = "Google";
+        job2._startYear = 2008;
+        job2._endYear = 2012;
+
+
+
+        job1.Display();
+        job2.Display();
     }
+    
+
 }
